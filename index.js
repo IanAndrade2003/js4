e65fc2069ba43bb6b1071cca7451ac728c1699e0
IanAndrade2003 <ianandrade90@outlook.com>
2021-05-25 12:13:37 -0300

    Final

---
+++ index.js
@@ -0,0 +1,29 @@
+function msg(){
+    console.log("Bem vindos ao mundo de hari póti")
+};
+
+msg()
+
+function nick(nome){
+    console.log("olá", nome)
+}
+
+nick("Ian")
+
+function info(years,style){
+    console.log("tenho",years,"anos e meu estilo é",style)
+}
+
+info(18,"todos menos funk")
+
+function gostos(film,music){
+    console.log("meu filme preferido é",film,"e minha musica preferida é",music)
+}
+
+gostos("Querido john","Hekireki - Last Alliance")
+
+function triple(num){
+    return num *3
+}
+
+console.log(triple(9))
